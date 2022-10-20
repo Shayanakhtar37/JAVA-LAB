fbbb7d2f485d2fd1569bb445f86870438a34c474
Shayan Akhtar <111417437+Shayanakhtar37@users.noreply.github.com>
2022-10-20 12:22:33 +0530

    Add files via upload

---
+++ 20th OCT/SwitchCase.java
@@ -0,0 +1,22 @@
+ /*
+Program: WAP to find the 1.square, 2. cube, 3. check number is even or odd using switch case."
+Author: Shayan Akhtar
+Date: 20th OCT 2022
+*/
+/ Importing Packages
+import java.util.Scannere;
+//Declaring a Class
+class SwitchCase
+{
+	static void calc(int n)
+	{
+		switch(n)
+		{
+			case 1 : System.out.println("Square");
+		          break;
+		    case 'B':System.out.println("Cube");
+		          break;
+		    case 'C':System.out.println("Even Or Odd");
+		          break;
+			default: System.out.println("Wrong input");
+		
